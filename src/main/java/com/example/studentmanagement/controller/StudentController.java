dc360bfd63f709f568fd43989670db766e830492
manickmanick <manickaperumal22@gmail.com>
2026-07-07 13:57:38 +0530

    created a student controller

---
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.studentmanagement.controller;
+
+import com.example.studentmanagement.service.StudentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+}
---
+++ src/main/java/com/example/studentmanagement/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.studentmanagement.service;
+
+import com.example.studentmanagement.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+}
